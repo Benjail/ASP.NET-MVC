816baaa3878f26afc522d19ce8f832d75e94c2dc
Benjail <miha203@mail.ru>
2019-11-10 13:54:13 +0300

    Настройка Startup, установка пакетов Static, MVC

--- Startup.cs
+++ Startup.cs
@@ -15,20 +15,24 @@ namespace ASP.NET_MVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(); //добавил концепцию MVC
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDeveloperExceptionPage(); //
+            app.UseStatusCodePages(); // 
+            app.UseStaticFiles(); //Статические файлы CSS, JS , IMG
+            app.UseMvcWithDefaultRoute(); //Дефолтный маршрут URL (HomeControler, Index.html)
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("In Development!");
+                });
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+        }
+            
         }
     }
-}
